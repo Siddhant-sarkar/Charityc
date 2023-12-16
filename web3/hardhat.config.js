a91a3064abb927f53a4b7cd2e5cfe394e624d339
Harsh-COE <harsh.coe8@gmail.com>
2023-12-17 00:46:16 +0530

    we are here at the point where the smart contracts are deployed

---
+++ web3/hardhat.config.js
@@ -0,0 +1,49 @@
+require("@matterlabs/hardhat-zksync-solc");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  networks: {
+    hardhat:{},
+    sepolia:{
+      url:'https://rpc.ankr.com/eth_sepolia',
+      accounts:[`a002c1c4296367dc94a86e879dc7a9c559d16e05cf39f49d66f7cada401cb973`],
+    },
+    zksync_testnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli",
+      chainId: 280,
+      zksync: true,
+    },
+    zksync_mainnet: {
+      url: "https://zksync2-mainnet.zksync.io/",
+      ethNetwork: "mainnet",
+      chainId: 324,
+      zksync: true,
+    },
+
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
